3a803050393aa6e7b4b8dcb64fa805d9d9b5c859
taco boi <tylerchall16@gmail.com>
2024-02-07 14:15:59 -0600

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,59 @@
-import { unstable_noStore as noStore } from "next/cache";
-import Link from "next/link";
-
-import { CreatePost } from "~/app/_components/create-post";
+// import { CreatePost } from "~/app/_components/create-post";
 import { api } from "~/trpc/server";
 
+type TNotesList = {
+  title: string;
+  body: string;
+  id: number;
+};
 export default async function Home() {
-  noStore();
   const hello = await api.post.hello.query({ text: "from tRPC" });
 
+  const notesList: TNotesList[] = [
+    {
+      title: "First note header",
+      body: "First note body",
+      id: 1,
+    },
+    {
+      title: "Second note header",
+      body: "Second note body",
+      id: 2,
+    },
+    {
+      title: "Third note header",
+      body: "Third note body",
+      id: 3,
+    },
+  ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+    <main className="bg-zinc-800">
+      <div className="container flex h-screen w-[30%] flex-row items-start border-r-2 border-solid  border-r-slate-500 align-top">
+        <ul className="flex flex-col gap-8">
+          {notesList.map((note) => (
+            <div key={note.id} className="">
+              <h1 className="font-bold text-gray-300">{note.title}</h1>
+              <p className="mt-4 text-gray-400">{note.body}</p>
             </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
-
-        <CrudShowcase />
+          ))}
+        </ul>
       </div>
     </main>
   );
 }
 
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest.query();
+// async function CrudShowcase() {
+//   const latestPost = await api.post.getLatest.query();
 
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
+//   return (
+//     <div className="w-full max-w-xs">
+//       {latestPost ? (
+//         <p className="truncate">Your most recent post: {latestPost.name}</p>
+//       ) : (
+//         <p>You have no posts yet.</p>
+//       )}
 
-      <CreatePost />
-    </div>
-  );
-}
+//       <CreatePost />
+//     </div>
+//   );
+// }
